b43dcbc20799f4f0e6ddb8694fad21be523ee63f
Herbert Straub <herbert@linuxhacker.at>
2015-04-06 00:58:28 +0200

    Extend the test sending loop.

--- src/main/scala/at/linuxhacker/kafka/actorSystem/TestApp.scala
+++ src/main/scala/at/linuxhacker/kafka/actorSystem/TestApp.scala
@@ -11,9 +11,11 @@ object TestApp extends App {
   val sender = system.actorOf( propsSender, "Sender" )
   val propsReceiver = Props( classOf[Receiver], "localhost:2181", "test", "akkaReveiver")
   val receiver = system.actorOf( propsReceiver, "Receiver" )
-  
-  for ( i <- 1 until 10 )
-    sender ! SendMessage( "test", "message nr " + i )
+  for ( x <- 1 until 10 ) {
+    for ( i <- 1 until 10 )
+      sender ! SendMessage( "test", "message nr " + i )
+    Thread.sleep( 5000 )
+  }
   
   Thread.sleep( 5000 )
   system.shutdown
